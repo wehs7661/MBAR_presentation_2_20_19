6e6c25b346ce6fca373a4734758431f65ad7eab7
Garrett Meek <garrett.a.meek@gmail.com>
2019-02-20 08:26:43 -0700

    More updates to free energy protocol

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # This script runs examples in subdirectories of the current folder.
 import sys
-sys.path.append("./NaCl")
 sys.path.append("./dice_example")
+sys.path.append("./NaCl/FreeEnergy")
 sys.path.append("./NaCl/PMF")
 import roll_dice as dice
-import openmmNaCl as NaCl
-import NaCl_analysis_MBAR as PMF
+import openmmNaCl as NaClFreeEnergy
+import NaCl_analysis_MBAR as NaClPMF
 
 exit()
